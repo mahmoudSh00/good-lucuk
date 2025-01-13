312ea4d06d84f6cae2c0816117d6beb06d396833
mahmoudSh00 <mohageer04@gmail.com>
2025-01-13 04:23:24 +0200

    Add files via upload

---
+++ config/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer fixed-bottom">
+    <strong>جميع الحقوق محفوظة &copy; <?php echo date('Y');?> 
+    <a href=https://www.facebook.com/alwahda.hospitel/?locale=ar_AR">مستشفى الوحدةالعلاجي / العيادات الخارجية</a>.</strong> 
+    <div class="float-right d-sm-block">
+    AlWaHDa HOSPITAL DERNA
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-blue">
+    <!-- Control sidebar content goes here -->
+  </aside>
